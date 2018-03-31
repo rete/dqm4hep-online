8a34c6986a605a8f0bb241c07e4f28876d7d0631
Remi Ete <remi.ete@gmail.com>
2018-04-01 00:43:48 +0200

    Transition to new json library from DQMCore

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -7,7 +7,7 @@ source dependencies/root/bin/thisroot.sh
 mkdir -p build
 cd build
 
-cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DJSONCPP_DIR=$PWD/../dependencies/jsoncpp/install -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake -DDQMCore_DIR=$PWD/../dependencies/dqm4hep-core -DDQMNet_DIR=$PWD/../dependencies/dqm4hep-net ..
+cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake -DDQMCore_DIR=$PWD/../dependencies/dqm4hep-core -DDQMNet_DIR=$PWD/../dependencies/dqm4hep-net ..
 
 if [ $? -ne 0 ]; then
     echo "Failed to run cmake"
